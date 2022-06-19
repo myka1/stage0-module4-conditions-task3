0117879609e5275903289bb9d82921fa91747b8b
myka1 <msmykolas@gmail.com>
2022-06-19 15:40:22 +0300

    changed

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
-        if(((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) && ((character != 'a' || character != 'e' || character != 'i' || character != 'o' || character != 'u' ) || (character != 'A' || character != 'E' || character != 'I' || character != 'O' || character != 'U' ))) {
+        if(((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) && character != 'a' && character != 'e' && character != 'i' && character != 'o' && character != 'u' && character != 'A' && character != 'E' && character != 'I' && character != 'O' && character != 'U' ) {
             System.out.println("Consonant");
-        } else if((character == 'a' || character == 'e' || character == 'i' || character == 'o' || character != 'u' ) || (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U' )) {
+        } else if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U' ) {
             System.out.println("Vowel");
         } else {
             System.out.println("wrong alphabet!");
